9c4ae9c0978b3a3f19fffc8b13e6c3c75fe9854b
Kelve T. Henrique <kelvehenrique@hotmail.com>
2018-05-23 13:28:00 +0200

    Define the endpoints

--- xmc4500/APP/BSP/bsp_gpio.c
+++ xmc4500/APP/BSP/bsp_gpio.c
@@ -16,23 +16,23 @@ _Bool BSP_GPIO_Init(void){
     P1_3_reset();
 
     // Initialise Endstop Pins
-    // Endstop left
-    P1_15_set_mode(OUTPUT_PP_AF3);
+    // Endstop top
+    P1_15_set_mode(INPUT);
     P1_15_set_driver_strength(MEDIUM);
     P1_15_reset();
 
-    // Endstop right
-    P1_13_set_mode(OUTPUT_PP_AF3);
+    // Endstop bottom
+    P1_13_set_mode(INPUT);
     P1_13_set_driver_strength(MEDIUM);
     P1_13_reset();
 
-    // Endstop top
-    P1_14_set_mode(OUTPUT_PP_AF3);
+    // Endstop left
+    P1_14_set_mode(INPUT);
     P1_14_set_driver_strength(MEDIUM);
     P1_14_reset();
 
-    // Endstop bottom
-    P1_12_set_mode(OUTPUT_PP_AF3);
+    // Endstop right
+    P1_12_set_mode(INPUT);
     P1_12_set_driver_strength(MEDIUM);
     P1_12_reset();
 
--- xmc4500/APP/BSP/bsp_gpio.h
+++ xmc4500/APP/BSP/bsp_gpio.h
@@ -14,6 +14,11 @@
 #define TICKS_PER_SECOND 1000
 #define TICKS_WAIT 500
 
+#define ENDRIGHT  P1_12_read()
+#define ENDLEFT   P1_14_read()
+#define ENDBOTTOM P1_13_read()
+#define ENDTOP    P1_15_read()
+
 _Bool BSP_GPIO_Init (void) ;
 
 #endif
